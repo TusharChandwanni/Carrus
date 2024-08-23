9cbbed49c50e346d96867f7d217a8f6240e33c96
Tushar Chandwanni <97699528+TusharChandwanni@users.noreply.github.com>
2024-08-24 00:05:37 +0530

    Update Jobs.jsx

--- frontend/src/components/Job/Jobs.jsx
+++ frontend/src/components/Job/Jobs.jsx
@@ -10,7 +10,7 @@ const Jobs = () => {
   useEffect(() => {
     try {
       axios
-        .get("/api/v1/job/getall", {port:4000},{
+        .get("/api/v1/job/getall",{
           withCredentials: true,
         })
         .then((res) => {
