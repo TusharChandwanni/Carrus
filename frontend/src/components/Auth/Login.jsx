bebd83c8d13b5cf430104dd448c969f750eda1b6
Tushar Chandwanni <97699528+TusharChandwanni@users.noreply.github.com>
2024-08-24 01:18:24 +0530

    Update Login.jsx

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "/api/v1/user/login",
+        "https://carrus.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
