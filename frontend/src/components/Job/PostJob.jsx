e04864856a3f3675be1c2d34798f400cc458ef22
Tushar Chandwanni <97699528+TusharChandwanni@users.noreply.github.com>
2024-08-24 00:06:35 +0530

    Update PostJob.jsx

--- frontend/src/components/Job/PostJob.jsx
+++ frontend/src/components/Job/PostJob.jsx
@@ -32,7 +32,7 @@ const PostJob = () => {
     }
     await axios
       .post(
-        "/api/v1/job/post",{port:4000},
+        "/api/v1/job/post",
         fixedSalary.length >= 4
           ? {
               title,
