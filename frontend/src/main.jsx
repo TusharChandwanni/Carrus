882147702849023e830595a09dbf3a92172c6ab3
Tushar Chandwanni <97699528+TusharChandwanni@users.noreply.github.com>
2024-08-23 23:25:13 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import axios from 'axios';
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 
 
 export const Context = createContext({
