0d32e4cfa5054cfbfb8ba852e14aae471f9a8997
Tushar Chandwanni <97699528+TusharChandwanni@users.noreply.github.com>
2024-08-23 23:41:48 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import axios from 'axios';
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.baseURL = 'https://carrus.onrender.com';
 
 
 export const Context = createContext({
